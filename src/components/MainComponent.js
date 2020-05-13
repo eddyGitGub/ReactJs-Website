597ac9ff3e37e72d81f6f525b3b0127b47436695
eddygitgub <eddycase1@gmail.com>
2020-05-13 20:14:51 +0100

    React router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,8 @@ import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -19,6 +21,14 @@ class Main extends Component {
   }
 
   render() {
+    const HomePage = () => {
+        return(
+            <Home 
+            />
+        );
+      }
+
+      
     return (
       <div>
         <Header />
